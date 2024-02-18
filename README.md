d6c6c07eb4f001334ead560cea9a617fdeee109e
sunshineguy <sunshine.dude.study@yandex.ru>
2024-02-19 02:19:05 +0300

    STUDY: Added first lesson from book

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project with HomeWork at learn Go With Tests  
+
+- Link to book: https://quii.gitbook.io/learn-go-with-tests
---
+++ helloworld/hello.go
@@ -0,0 +1,34 @@
+package hello
+
+const (
+	french  = "French"
+	spanish = "Spanish"
+	russian = "Russian"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+	russianHelloPrefix = "Привет, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	case russian:
+		prefix = russianHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
