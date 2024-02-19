2955a13e4882b94c4b468ee660fe3c0bfb4b3567
sunshineguy <sunshine.dude.study@yandex.ru>
2024-02-20 01:35:24 +0300

    iterations and slices chapter

---
+++ arrays/sum.go
@@ -0,0 +1,23 @@
+package arrays
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+			continue
+		}
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+
+	return sums
+}
